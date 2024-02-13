1fb62aa86219f7e396f58cea610e0a2f97686cab
NatCanCode <nathalie.piat1@gmail.com>
2024-02-13 21:10:36 +0100

    Update StoryBoard as TableView

--- UIKitToDoApp/ViewController.swift
+++ UIKitToDoApp/ViewController.swift
@@ -7,13 +7,35 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UITableViewController {
+
+    // MARK: Lifecycle
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+
+       configureTableView()
     }
 
+    // MARK: Helper functions
 
+    func configureTableView() {
+        tableView.backgroundColor = .lightGray
+
+    }
 }
 
+// MARK: UITableViewDelegate / UITableViewDataSource
+
+extension ViewController {
+
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 5
+    }
+
+    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "ToDoCell", for: indexPath)
+
+        return cell // Display 5 cells
+    }
+}
